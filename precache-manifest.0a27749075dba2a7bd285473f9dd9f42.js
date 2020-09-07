4446162d00ec93dcae782d7fb1a13b96b7843e28
Lucas Castro <lucascastro102@gmail.com>
2020-09-07 17:12:04 -0300

    Updates

---
+++ precache-manifest.0a27749075dba2a7bd285473f9dd9f42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eba75538291a8016a8fd94199378f84b",
+    "url": "/snake-game/index.html"
+  },
+  {
+    "revision": "680b40b551994414dcb6",
+    "url": "/snake-game/static/css/main.83d5a3f2.chunk.css"
+  },
+  {
+    "revision": "b0fc05d25d07c7be7cf2",
+    "url": "/snake-game/static/js/2.93846d94.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/snake-game/static/js/2.93846d94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "680b40b551994414dcb6",
+    "url": "/snake-game/static/js/main.4d8c4cfc.chunk.js"
+  },
+  {
+    "revision": "18005e22df855f84fc12",
+    "url": "/snake-game/static/js/runtime-main.f9e1bf59.js"
+  },
+  {
+    "revision": "440b53b1a1c65037f944ff19259d8014",
+    "url": "/snake-game/static/media/nokiafc22.440b53b1.ttf"
+  }
+]);
